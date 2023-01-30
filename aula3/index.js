e761323d42dd5d8e364d1bb42e8baa961bf05967
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-30 20:49:13 -0300

    tipagem primitiva

---
+++ aula3/index.js
@@ -0,0 +1,9 @@
+//Tipos Primitivoss
+const nome = 'Gabriel'//String
+const nome2 = "Gabriel"//String
+const nome3 = `Gabriel`//String
+
+const num1 = 10;
+
+let nomeTeste; //Undefined -- Ainda não foi definido valor
+let nomeTeste2 = null//Não aponta para lugar nenhum na memória, não existe simplesmente
