14af4bdef926f6bea1e35fa321e9eed2b39f0cdc
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-30 20:35:30 -0300

    add const, let, var

---
+++ aula2/index.js
@@ -0,0 +1,19 @@
+//variáveis precisam ter nomes significativos
+//Utilize let, const não var
+
+let nome = 'Gabriel'//string
+
+console.log(`${nome} tem 18 anos!`)
+console.log(`Hoje é o aniversário de ${nome}`)
+
+//A constante não pode ter seu valor modificado
+
+const teste = 'João'
+
+const primeiro = 6
+const segundo = 5
+const resultado = primeiro * segundo
+const resultadoTriplicado = resultado * 3
+
+console.log(resultado)
+console.log(resultadoTriplicado)
