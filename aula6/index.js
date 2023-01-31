78afe324f6d5b1678c5e83f6bf05a38a0b5fde5b
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-31 11:22:03 -0300

    add coisas sobre strings

--- aula6/index.js
+++ aula6/index.js
@@ -4,6 +4,17 @@ let umaString = '"Um texto"'//Você pode por aspas dentro de aspas
 console.log(umaString)
 
 //As strings são determinadas com índices
-//           12345678912345
+//           0123456789
 let teste = 'Qualquer texto'
 console.log(teste[5])
+
+console.log(`${umaString} e ${teste}`)
+console.log(umaString.indexOf('x'))//Acha a posição do index de que vc procura
+
+let umTexto = 'Um texto qualquer'
+console.log(umTexto.replace('qualquer', 'camarada')) //Substitui baseado na palavra escolhida
+
+console.log(umTexto.length)
+
+console.log(umTexto.slice(2,6))//Pega a parte baseada nos indices
+console.log(umaString.split('q'))
