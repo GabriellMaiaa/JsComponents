b07c0a142b4fc3dba0446ffbfe38e9f8b07071f9
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-30 21:52:55 -0300

    introducao operadores

--- aula4/index.js
+++ aula4/index.js
@@ -7,3 +7,19 @@ console.log(num1 ** num2)// Elevação ao quadrado
 console.log(num1 % num2)//Pega o resto da conta
 
 console.log((num1 + num2) * num3) //O parênteses dá a precedência de quem estiver dentro dele
+
+//Incremento ++ e Decrememto --
+
+let contador = 0
+const passo =2 
+
+contador = passo + contador;
+console.log(contador)
+contador = passo + contador;
+console.log(contador)
+contador = passo + contador;
+console.log(contador)
+
+//ouuu
+
+contador+= passo;
