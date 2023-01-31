2a6b1847fa7556c9fc8223e843d9eafa9fc6a30f
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-31 11:02:15 -0300

    add mais coisas

---
+++ aula6/index.js
@@ -0,0 +1,9 @@
+//Um pouco mais sobre Strings
+
+let umaString = '"Um texto"'//Você pode por aspas dentro de aspas 
+console.log(umaString)
+
+//As strings são determinadas com índices
+//           12345678912345
+let teste = 'Qualquer texto'
+console.log(teste[5])
