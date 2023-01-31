496ee514a7d9b4ca35a7e22058982cfbbd9479e0
Gabriel Maia <gribeiromaia736@gmail.com>
2023-01-30 21:36:06 -0300

    operadores

--- aula3/index.js
+++ aula3/index.js
@@ -7,3 +7,9 @@ const num1 = 10;
 
 let nomeTeste; //Undefined -- Ainda não foi definido valor
 let nomeTeste2 = null//Não aponta para lugar nenhum na memória, não existe simplesmente
+
+const aprovado = true; //Booleano - Operador lógico, true ou false
+
+console.log(typeof aprovado, nomeTeste)
+
+//string, number, boolean, null e undefined
---
+++ aula4/index.js
@@ -0,0 +1,9 @@
+// Operadores Aritméticos +, -
+const num1 = 10
+const num2= 5
+const num3 = 15
+console.log(num1 * num2)
+console.log(num1 ** num2)// Elevação ao quadrado
+console.log(num1 % num2)//Pega o resto da conta
+
+console.log((num1 + num2) * num3) //O parênteses dá a precedência de quem estiver dentro dele
