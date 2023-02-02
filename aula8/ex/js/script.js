ddde3b2a4a21e2334d32f8ba8b24192cb011094a
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-02 17:18:29 -0300

    add mais

--- aula8/ex/js/script.js
+++ aula8/ex/js/script.js
@@ -1,2 +1,10 @@
-const numero = prompt('Digite um número');
-document.getElementById('numero-titulo');
+const numero = Number(prompt('Digite um número'));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p> Seu número -2 é ${numero -2 }</p>`
+texto.innerHTML = `<p> Raiz quadrada ${numero ** 0.5 }</p>`
+texto.innerHTML = `<p>${numero}inteiro ${Number.isInteger(numero)}</p>`
+texto.innerHTML = `<p> Arredondando para baixo é ${Math.floor(numero)}</p>`
+texto.innerHTML = `<p> Arredondando para baixo é ${Math.ceil(numero)}</p>`
