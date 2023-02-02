362accedad8678e4eba36bbc1dc31301794f7aa1
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-02 17:22:16 -0300

    add

--- aula8/ex/js/script.js
+++ aula8/ex/js/script.js
@@ -3,8 +3,9 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p> Seu número -2 é ${numero -2 }</p>`
-texto.innerHTML = `<p> Raiz quadrada ${numero ** 0.5 }</p>`
-texto.innerHTML = `<p>${numero}inteiro ${Number.isInteger(numero)}</p>`
-texto.innerHTML = `<p> Arredondando para baixo é ${Math.floor(numero)}</p>`
-texto.innerHTML = `<p> Arredondando para baixo é ${Math.ceil(numero)}</p>`
+texto.innerHTML += `<p> Seu número -2 é ${numero -2 }</p>`
+texto.innerHTML += `<p> Raiz quadrada ${numero ** 0.5 }</p>`
+texto.innerHTML += `<p>${numero} inteiro = ${Number.isInteger(numero)}</p>`
+texto.innerHTML += `<p> Arredondando para baixo é ${Math.floor(numero)}</p>`
+texto.innerHTML += `<p> Arredondando para cima é ${Math.ceil(numero)}</p>`
+texto.innerHTML += `<p> Com duas casas decimais ${numero.toFixed(2)}}</p>`
