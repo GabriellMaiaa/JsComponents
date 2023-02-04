bc9df47a0cacedea19bbb412ac989068fa8a369f
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-04 12:28:03 -0300

    iniciando arrays

--- aula8/ex/js/script.js
+++ aula8/ex/js/script.js
@@ -9,3 +9,5 @@ texto.innerHTML += `<p>${numero} inteiro = ${Number.isInteger(numero)}</p>`
 texto.innerHTML += `<p> Arredondando para baixo é ${Math.floor(numero)}</p>`
 texto.innerHTML += `<p> Arredondando para cima é ${Math.ceil(numero)}</p>`
 texto.innerHTML += `<p> Com duas casas decimais ${numero.toFixed(2)}}</p>`
+
+//Tudo isso é para adicionar texto no HTML através dop DOM
---
+++ aula9/index.js
@@ -0,0 +1,4 @@
+//ARRAYS - Coleção de dados, variável com vários valores
+
+const alunos =['GABRIEL', 'FERNANDA', 'MARCOS', 'MARIA']
+console.log(alunos)
