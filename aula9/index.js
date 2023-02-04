298f0fecc55d3ede08047829e00fb0d24cbe379a
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-04 12:37:37 -0300

    funcoes de arrays

--- aula9/index.js
+++ aula9/index.js
@@ -2,3 +2,15 @@
 
 const alunos =['GABRIEL', 'FERNANDA', 'MARCOS', 'MARIA']
 console.log(alunos)
+console.log(alunos[2])
+//Se eu quiser editar o arrray 
+alunos[1] = 'KARLA'// Aqui altera de acordo com o indice do Array
+console.log(alunos)
+console.log(alunos.length)
+
+//Para adicionar elementos ao array usa-se o PUSH
+alunos.push = ('MAIA');
+console.log(alunos)
+//Para adicionar no início usa-se o UNSHIFT
+alunos.unshift("FÁBIO");
+console.log(alunos)
