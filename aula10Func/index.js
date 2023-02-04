968a0dc3de138ef658a4c846a2a3bd0ccfa72335
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-04 13:24:55 -0300

    entendendo

--- aula10Func/index.js
+++ aula10Func/index.js
@@ -1,4 +1,13 @@
 function saudacao (nome) {
-    console.log(`Bom dia senhor ${nome}!`)  
+    return(`Bom dia senhor ${nome}!`)  //Se põe sua mensagem no retorno da função
 }
-saudacao('Gabriel Maia')
+const variavel = saudacao('Gabriel Maia');
+console.log(variavel)
+
+function soma (x, y) {
+    const resultado = x + y ;// Não é possível alterar variaveis de dentro das funções
+    return resultado;
+}
+console.log(soma(2, 4))
+const resultado = soma(14,4);
+console.log(`A idade de Gabriel é ${resultado}`);
