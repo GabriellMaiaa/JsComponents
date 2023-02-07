bc6b1c6176b09ae4a5c1f5274d533e2134b833bc
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-07 09:40:32 -0300

    objetos

---
+++ aula11Obj/index.js
@@ -0,0 +1,10 @@
+//Objetos básico
+const array = [1,2,3]
+array.push('Gabriel')
+console.log(array)
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Castro'
+}
+console.log(pessoa1)
--- aula8/ex/js/script.js
+++ aula8/ex/js/script.js
@@ -2,7 +2,7 @@ const numero = Number(prompt('Digite um número'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
-numeroTitulo.innerHTML = numero;
+numeroTitulo.innerHTML = numero;// Isso faz com que o que for digitado apareça por meio do DOM
 texto.innerHTML += `<p> Seu número -2 é ${numero -2 }</p>`
 texto.innerHTML += `<p> Raiz quadrada ${numero ** 0.5 }</p>`
 texto.innerHTML += `<p>${numero} inteiro = ${Number.isInteger(numero)}</p>`
