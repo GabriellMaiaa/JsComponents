8beb58aa809433d408acd4cc506a0d94d50d8761
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-08 11:02:57 -0300

    int a condicionais

---
+++ aula14If/index.js
@@ -0,0 +1,13 @@
+//O if pode ser usado sozinho
+// O Else necessita de um if antes
+
+const hora = 29;
+if(hora >= 0 && hora <= 11) {// Hora entre 0 e 11h
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17){ // O ELSE IF serve quando vocÊ quer que tenhya mais uma condição
+    console.log('Boa tarde!')
+} else if(hora >= 18 && hora <= 23) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá, este horário não existe!')
+}
