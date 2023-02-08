755cbd20f23433cb17268922e92527992146ae42
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-08 09:50:19 -0300

    operadore

---
+++ aula13Log/iindex.js
@@ -0,0 +1,8 @@
+// Operadores de Lógicos
+/**
+ * && - AND, E -> As duas expressões tem que ser verdadeiras
+ * || - OR, OU -> Uma ou outra precisa ser verdadeira
+ * ! - NOT, NEGAÇÃO
+ */
+const expAnd = true && true && false;
+console.log(expAnd)
