1d98b5ce5248f25ced56fe16e98681aab4a89b4e
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-10 10:59:57 -0300

    for in

---
+++ aula20ForIn/index.js
@@ -0,0 +1,22 @@
+//Vetor - Array de somente uma sequência
+const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+// FOR IN abaixo, é interessante em arrays de objetos
+// Ele lê os índices(vetor) ou chaves do objeto
+for(let index in frutas) {
+    console.log(frutas[index])
+}
+// Acessando com o for in OBJETOS
+
+const pessoa = {
+    nome: 'Gabriel',
+    sobrenome: "Maia",
+    idade: 19
+}
+for (let i in pessoa) {
+    console.log(pessoa[i])// Com objetos é da mesma forma
+}// Se quiser acessar somente o nome da opção, basta deixar só o I
+console.log(pessoa.nome)
