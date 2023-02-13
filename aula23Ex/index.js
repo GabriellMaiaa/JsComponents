ecaca94e7db5f802769d3cb62006775aa85f6903
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-13 11:34:43 -0300

    exercicios

---
+++ aula23Ex/index.js
@@ -0,0 +1,15 @@
+/** Escreva uma função que receba 2 numeros e
+ * retorne o maior etre eles.*/ 
+function max (x, y) {
+   return x > y ? x : y;
+}
+const max2 = (x, y) => { x > y ? x : y};
+
+/** if(x > y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+ */
+console.log(max(102, 34))
