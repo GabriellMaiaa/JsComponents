456765ab74a25a3e74aa058bba4225b89b1afd15
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-14 11:29:59 -0300

    try

--- aula24TryCatch/index.js
+++ aula24TryCatch/index.js
@@ -7,12 +7,12 @@ try {
 }
 function soma(x, y) {
     if(typeof x !== 'number' || typeof y !== 'number') {
-        throw new Error('X e Y precisam ser números')
+        throw new Error('X e Y precisam ser números')// Lançar o erro que pode ocorrer
     }
     return x + y
 }
-try {
-    console.log(soma('10, 45));
+try {// Como capturar o Erro
+    console.log(soma(10, 45));// Se n ocorrer erro no try, o catch pegará
 } catch(error) {
    console.log(error) 
 }
