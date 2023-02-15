46fc8e7ae39e2d1f5702fce30219d941a7148fea
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-15 09:10:29 -0300

    func

---
+++ aula26Func/index.js
@@ -0,0 +1,14 @@
+function funcao() {
+    console.log('Oiee')
+}
+funcao('valor')// Deveria dar erro por não ter parâmetros, mas o JS não está nem aí 
+
+function teste() {
+   let total = 0
+   for(let argumento of arguments){
+    total += argumento
+   }
+   
+    console.log(total)
+}
+teste(1,2,3,4,5,6)//Esses valores são salvos no argumento
