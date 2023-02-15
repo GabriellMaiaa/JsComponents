9c25c9c948a108c25509d047caf49f32effa17b0
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-15 09:21:17 -0300

    param de func

--- aula26Func/index.js
+++ aula26Func/index.js
@@ -3,12 +3,15 @@ function funcao() {
 }
 funcao('valor')// Deveria dar erro por não ter parâmetros, mas o JS não está nem aí 
 
-function teste() {
+function teste() {//São os Arguments
    let total = 0
    for(let argumento of arguments){
     total += argumento
    }
-   
-    console.log(total)
+    console.log(total) 
 }
 teste(1,2,3,4,5,6)//Esses valores são salvos no argumento
+
+function conta(operador, acumulador, ...numeros) {// Rest Operator- Tudo depois fica
+    console.log(operador, acumulador, numeros);// dentro do Array
+}
