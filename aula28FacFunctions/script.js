db5fd7f76d6a95ce466c117d54c94ca654fdd696
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-24 11:56:43 -0300

    usando get

--- aula28FacFunctions/script.js
+++ aula28FacFunctions/script.js
@@ -1,8 +1,19 @@
 //São funções que retornam objetoss
-function criaPessoa(nome, sobrenome) {
+// Factory Functions - Dá pra por funções dentro dos objetos
+function criaPessoa(nome, sobrenome,a , p) {
     return {
-        nome, sobrenome
+        nome, 
+        sobrenome,
+        fala(assunto) {
+            return `${this.nome} está falando sobre ${assunto}`
+        },
+        altura: a,
+        peso:p,
+        get imc() {// O GET torna o imc funcção como se fosse um ATRIBUTO.
+            const indice = this.peso / (this.altura ** 2);
+            return indice;
+        }
     }
-}
-const p1= criaPessoa('Gabriel', 'Maia')
-console.log(p1)
+}// O THIS serve para chamar aquele argumento do objeto que é chamado
+const p1= criaPessoa('Gabriel', 'Maia',1.90, 90 );
+console.log(p1.imc)// O get o tornou um atributo normal
